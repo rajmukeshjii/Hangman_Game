7c2037d33d1bae743f11bd10611d5427db0ca724
MUKESH RAJ <119166795+rajmukeshjii@users.noreply.github.com>
2025-01-17 16:27:33 +0530

    initial

---
+++ hangman_game.py
@@ -0,0 +1,28 @@
+import random
+
+word_list=["apple","beautiful","potato"]
+lives=6
+chosen_word=random.choice(word_list)
+print(chosen_word)
+display=[]
+for letter in range (len(chosen_word)):
+    display+="_"
+print(display)
+game_over=False
+while not game_over:
+    guessed_letter=input("guess a letter:").lower()
+    for position in range(len(chosen_word)): 
+        letter=chosen_word[position]
+        if letter==guessed_letter:
+            display[position] = guessed_letter
+    print(display)
+    
+    if guessed_letter not in chosen_word: 
+        lives-=1
+        if lives==0:
+            game_over=True
+            print("you lose!!")
+    if "_" not in display:
+        game_over=True
+        print("you win!!")
+    
